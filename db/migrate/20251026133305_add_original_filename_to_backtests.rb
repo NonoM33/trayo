28c499b535790f97687a67d88be9e9235d6a5d01
cosson renaud <renaud.cosson-ext@wifirst.fr>
2025-10-26 15:54:27 +0100

    Fix migration for original_filename in backtests

--- db/migrate/20251026133305_add_original_filename_to_backtests.rb
+++ db/migrate/20251026133305_add_original_filename_to_backtests.rb
@@ -1,5 +1,7 @@
 class AddOriginalFilenameToBacktests < ActiveRecord::Migration[8.0]
   def change
-    add_column :backtests, :original_filename, :string
+    unless column_exists?(:backtests, :original_filename)
+      add_column :backtests, :original_filename, :string
+    end
   end
 end
