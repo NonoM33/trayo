cfaac3b5ddc5296bc8e21ced0061e76eeb90d447
cosson renaud <renaud.cosson-ext@wifirst.fr>
2025-10-26 13:44:29 +0100

    feat: add format_duration methods and action button styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def format_duration(total_minutes)
+    return "<span style='color: var(--text-muted);'>-</span>".html_safe if total_minutes.nil? || total_minutes == 0
+    
+    total_minutes = total_minutes.to_f
+    
+    if total_minutes >= 1440
+      days = (total_minutes / 1440.0).round(1)
+      "#{days} #{days > 1 ? 'jours' : 'jour'}"
+    elsif total_minutes >= 60
+      hours = (total_minutes / 60.0).round(1)
+      "#{hours}h"
+    else
+      "#{total_minutes.round(0)} min"
+    end
+  end
+  
+  def format_duration_from_hours(hours)
+    return "<span style='color: var(--text-muted);'>-</span>".html_safe if hours.nil? || hours == 0
+    
+    hours = hours.to_f
+    
+    if hours >= 24
+      days = hours.round(1)
+      "#{days} #{days > 1 ? 'jours' : 'jour'}"
+    else
+      "#{hours.round(1)}h"
+    end
+  end
 end
