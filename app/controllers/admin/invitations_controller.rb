7ca5124c4f5f44b27baa1a70f6dcc31f2861ad24
cosson renaud <renaud.cosson-ext@wifirst.fr>
2025-11-28 20:51:16 +0100

    refactor: refactor and improve display of invitation details

--- app/controllers/admin/invitations_controller.rb
+++ app/controllers/admin/invitations_controller.rb
@@ -2,35 +2,23 @@ module Admin
   class InvitationsController < BaseController
     before_action :require_admin
     
-    def index
-      @invitations = Invitation.order(created_at: :desc).page(params[:page]).per(20)
-    end
-    
-    def new
-      @invitation = Invitation.new
-    end
-    
     def create
       @invitation = Invitation.new(invitation_params)
       @invitation.code = Invitation.generate_unique_code
       @invitation.expires_at = 30.days.from_now unless @invitation.expires_at
       
       if @invitation.save
-        redirect_to admin_invitation_path(@invitation), notice: "Invitation créée avec succès"
+        redirect_to admin_clients_path, notice: "Invitation créée ! Code: #{@invitation.code}"
       else
-        render :new, status: :unprocessable_entity
+        redirect_to admin_clients_path, alert: "Erreur lors de la création de l'invitation"
       end
-    end
-    
-    def show
-      @invitation = Invitation.find(params[:id])
     end
     
     def destroy
       @invitation = Invitation.find(params[:id])
       @invitation.destroy
       
-      redirect_to admin_invitations_path, notice: "Invitation supprimée"
+      redirect_to admin_clients_path, notice: "Invitation supprimée"
     end
     
     private
