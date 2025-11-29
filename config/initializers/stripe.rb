134774a3e84cff9864f1f56a63009742de17c594
cosson renaud <renaud.cosson-ext@wifirst.fr>
2025-11-29 15:10:52 +0100

    refactor: move Stripe keys to .env file

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,23 +1,18 @@
 # Configuration Stripe
-# Les clés doivent être définies via les variables d'environnement:
+# Les clés doivent être définies via les variables d'environnement (.env):
 # - STRIPE_SECRET_KEY
 # - STRIPE_PUBLISHABLE_KEY
 
-STRIPE_TEST_SECRET_KEY = "sk_test_REMOVED".freeze
-STRIPE_TEST_PUBLISHABLE_KEY = "pk_test_REMOVED".freeze
-
-Stripe.api_key = ENV.fetch('STRIPE_SECRET_KEY') do
-  if Rails.env.production?
-    raise "STRIPE_SECRET_KEY must be set in production environment"
-  else
-    STRIPE_TEST_SECRET_KEY
-  end
+if ENV['STRIPE_SECRET_KEY'].present?
+  Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+elsif Rails.env.production?
+  raise "STRIPE_SECRET_KEY must be set in production environment"
 end
 
-Rails.application.config.stripe_publishable_key = ENV.fetch('STRIPE_PUBLISHABLE_KEY') do
-  if Rails.env.production?
-    raise "STRIPE_PUBLISHABLE_KEY must be set in production environment"
-  else
-    STRIPE_TEST_PUBLISHABLE_KEY
-  end
+if ENV['STRIPE_PUBLISHABLE_KEY'].present?
+  Rails.application.config.stripe_publishable_key = ENV['STRIPE_PUBLISHABLE_KEY']
+elsif Rails.env.production?
+  raise "STRIPE_PUBLISHABLE_KEY must be set in production environment"
+else
+  Rails.application.config.stripe_publishable_key = nil
 end
